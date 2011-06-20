38de8cdfd7508bd12141aabb072bc2c3e0f450dd
MatmaRex <matma.rex@gmail.com>
2011-06-20 22:16:20 +0200

    fixup for new Sunflower

--- scripts/recat.rb
+++ scripts/recat.rb
@@ -1,32 +1,27 @@
-require 'sunflower-commontasks.rb'
-require 'sunflower-listmaker.rb'
+# coding: utf-8
+require 'sunflower'
 
-from='Warhammer 40000'
-to='Warhammer 40.000'
+from = ''
+to   = ''
 
-s=Sunflower.new
-s.login
-print "Logged in!\n"
+s = Sunflower.new.login
 
 print "Reading articles list... "
-list=s.make_list('category-r', 'Kategoria:'+from).sort
+list=s.make_list('category', 'Kategoria:'+from).sort
 print "done!\n\n"
 
-# EDIT SUMMARY BELOW
-$summary='zmiana nazewnictwa: Warhammer 40000 -> 40.000, [[WP:SK]]'
+s.summary = ''
 
 list.each do |title|
 	print "Reading page #{title}... "
 	page=Page.get(title)
 	print "modifying... "
 
-	page.codeCleanup
-	page.changeCategory from, to
+	page.code_cleanup
+	page.change_category from, to
 	
 	print "saving... "
 	page.save
 	print "done!\n\n"
 end
 
-print 'Finished! Press any key to close.'
-gets
