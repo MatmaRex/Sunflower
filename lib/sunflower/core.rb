cdc5e5194ff80a20f35e731a13dbbbfa2faf7f5d
MatmaRex <matma.rex@gmail.com>
2012-10-12 17:55:40 +0200

    version bump (0.5.4)

--- lib/sunflower/core.rb
+++ lib/sunflower/core.rb
@@ -17,7 +17,7 @@ require 'cgi'
 #
 # You can use multiple Sunflowers at once, to work on multiple wikis.
 class Sunflower
-	VERSION = '0.5.3'
+	VERSION = '0.5.4'
 	
 	INVALID_CHARS = %w(# < > [ ] | { })
 	INVALID_CHARS_REGEX = Regexp.union *INVALID_CHARS
