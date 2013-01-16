250bb4f9a6e07c54c1ae83091af41faa6609447e
MatmaRex <matma.rex@gmail.com>
2013-01-16 16:47:41 +0100

    fix crash on login when wiki has only one user

--- lib/sunflower/core.rb
+++ lib/sunflower/core.rb
@@ -327,7 +327,7 @@ class Sunflower
 		
 		# 4. check bot rights
 		r=self.API('action=query&list=allusers&aulimit=1&augroup=bot&aufrom='+(CGI.escape user))
-		unless r['query']['allusers'][0]['name']==user
+		unless r['query']['allusers'][0] && r['query']['allusers'][0]['name']==user
 			warn 'Sunflower - this user does not have bot rights!' if @warnings
 			@is_bot=false
 		else
